1f8166270a8e30858c519f6379dc15d817928863
Naroueporn Charoenthawornnon <naroueporn.cha@student.mahidol.com>
2026-09-03 12:54:27 +0700

    A5: RED test_grams_are_not_ounces()

--- test_kitchen.py
+++ test_kitchen.py
@@ -29,4 +29,6 @@ def test_equality():
     assert Quantity(200) == Quantity(200)
     assert Quantity(200) != Quantity(300)
 
+def test_grams_are_not_ounces():
+    assert Quantity(1, "g") != Quantity(1, "oz")
 
