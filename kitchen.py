183e9bd03357a02f573eba0ca28d29fde0b6090b
Naroueporn Charoenthawornnon <naroueporn.cha@student.mahidol.com>
2026-09-09 12:07:12 +0700

    Revised again to correctly align with the requirements.

--- kitchen.py
+++ kitchen.py
@@ -1,15 +1,1 @@
 # kitchen.py
-class Quantity:
-    def __init__(self, amount):
-        self.amount = amount
- 
-    def times(self, multiplier):
-        self.amount = 600
-
-    def times(self, multiplier):
-        self.amount = self.amount * multiplier
-
-    
-
-
-        
