40542d5237f2a90cc4161882d5c414fec5f40bc3
Naroueporn Charoenthawornnon <naroueporn.cha@student.mahidol.com>
2026-09-09 12:24:25 +0700

    A3: refactored tests, GREEN

--- test_kitchen.py
+++ test_kitchen.py
@@ -9,16 +9,6 @@
 
 # test_kitchen.py
 from kitchen import Quantity
- 
-def test_multiplication():
-    flour = Quantity(200)
-    flour.times(3)
-    assert flour.amount == 600
-
-def test_multiplication_by_two():
-    flour = Quantity(200)
-    flour.times(2)
-    assert flour.amount == 400
 
 def test_multiplication_returns_a_new_quantity():
     flour = Quantity(200)
